f2fac28ce4335d6dafc8da484faae301609e2002
SIC98 <shininchul98@gmail.com>
2023-08-12 16:25:56 +0900

    Add karlo api

---
+++ karlo_api.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import urllib
+from PIL import Image
+
+REST_API_KEY = 'c53a3bd9609b7a2dabc7d1a95083248f'
+
+
+def t2i(prompt, negative_prompt):
+    r = requests.post(
+        'https://api.kakaobrain.com/v2/inference/karlo/t2i',
+        json={
+            'prompt': prompt,
+            'negative_prompt': negative_prompt
+        },
+        headers={
+            'Authorization': f'KakaoAK {REST_API_KEY}',
+            'Content-Type': 'application/json'
+        }
+    )
+
+    response = json.loads(r.content)
+    return response
