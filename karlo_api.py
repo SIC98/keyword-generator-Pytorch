b7c4c1fe98ee3a003bdf903018c0eae64d55a157
SIC98 <shininchul98@gmail.com>
2023-08-17 17:53:30 +0900

    Use double quote

--- karlo_api.py
+++ karlo_api.py
@@ -6,14 +6,14 @@ REST_API_KEY = "REST_API_KEY"
 
 def t2i(prompt, negative_prompt):
     r = requests.post(
-        'https://api.kakaobrain.com/v2/inference/karlo/t2i',
+        "https://api.kakaobrain.com/v2/inference/karlo/t2i",
         json={
-            'prompt': prompt,
-            'negative_prompt': negative_prompt
+            "prompt": prompt,
+            "negative_prompt": negative_prompt
         },
         headers={
-            'Authorization': f'KakaoAK {REST_API_KEY}',
-            'Content-Type': 'application/json'
+            "Authorization": f"KakaoAK {REST_API_KEY}",
+            "Content-Type": "application/json"
         }
     )
 
